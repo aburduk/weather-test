83a15df37956d2f25d9e6744cc08f3c855481414
aburduk <artem.burduk@gmail.com>
2021-07-18 21:45:44 +0300

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 //OPEN WEATHER
 export const OPEN_WEATHER_API_URL =
   'https://api.openweathermap.org/data/2.5/onecall?';
-export const OPEN_WEATHER_API_KEY = '0dd23cd8d5ab2a876c1cd23313778f65';
+export const OPEN_WEATHER_API_KEY = 'API_KEY';
 
 // GOOGLE INTEGRATION
-export const GOOGLE_API_KEY = 'AIzaSyACEJX3jCJiKik5kMqp4UjlK4PA1xmCWGw';
+export const GOOGLE_API_KEY = 'GOOGLE_API_KEY';
